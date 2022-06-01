9a87f8c5ed970b2bf60c8662d2526229f62021b9
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2022-05-31 22:22:53 -0500

    Optimization

--- src/model/threads/THAlien.java
+++ src/model/threads/THAlien.java
@@ -3,12 +3,9 @@ package model.threads;
 import model.gameObjects.Alien;
 import model.gameObjects.MovingObject;
 
-import java.awt.*;
-
 public class THAlien extends Thread {
     private Alien alien;
     private boolean running;
-    private Thread thread;
 
     public THAlien(Alien alien) {
         this.alien = alien;
@@ -20,28 +17,9 @@ public class THAlien extends Thread {
         while (running) {
             alien.update();
 
-            new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        while (running) {
-                            double numerousAleatory = (Math.random() * 100);
-
-                            if (numerousAleatory < 0.25) {
-                                alien.shoot();
-                            }
-
-                            try {
-                                Thread.sleep(5000);
-                            }catch (InterruptedException e){
-                                e.printStackTrace();
-                            }
-
-
-                        }
-
-                    }
-            }).start();
-
+            if ((Math.random() * 100) < 1.5) {
+                alien.shoot();
+            }
 
             try {
                 Thread.sleep(25);
